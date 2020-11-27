a5e36a691dc4c19280573a66a1d4348b19a978e3
ballo <ballo.av@edu.spbstu.ru>
2020-11-28 00:12:46 +0300

    markdownToHtmlSimple was fixed

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -341,7 +341,7 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
     writer.write("<html>" + "\n")
     writer.write("<body>" + "\n")
     writer.write("<p>" + "\n")
-    val stack = mutableListOf(-1) // 0 в листе, т.к. <p> уже записано
+    val stack = mutableListOf(-1, 0) // 0 в листе, т.к. <p> уже записано
     //-1 - для проверки stack.last == smthng, иначе ошибка ListEmpty
     /*
     Условные обозачения тэгов в стэке
@@ -350,7 +350,7 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
     2 - <b> - **
     3 - <s> - ~~
     */
-    val listOfLines = File(inputName).readText().trim('\n').split("\r\n")
+    val listOfLines = File(inputName).readLines()
     var i = 0
     for (line in listOfLines) {
         if (line.isEmpty()) {
